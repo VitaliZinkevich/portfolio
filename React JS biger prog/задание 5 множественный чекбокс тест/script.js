b4e1cee82f21d614182e7ec451f501c4d2a4ee79
Vitali <31952551+VitaliZinkevich@users.noreply.github.com>
2017-11-01 17:50:54 +0300

    проверка ответов и вывод
    готово

--- React JS biger prog/задание 5 множественный чекбокс тест/script.js
+++ React JS biger prog/задание 5 множественный чекбокс тест/script.js
@@ -1,4 +1,4 @@
-/*  Модифицируйте предыдущую задачу так, чтобы пользователь мог выбрать несколько 
+/*  Модифицируйте предыдущую задачу так, чтобы пользователь мог выбрать несколько
 вариантов ответа с помощью 4-х чекбоксов. */
 
 // проверка ответов доделать
@@ -19,19 +19,19 @@ constructor() {
             const questionsList = this.props.question.map ((elem,indexQ)=>{
                 return (
 
-                <ol className={this.props.viewQuestions[indexQ] == true ? "d-block" : "d-none" } 
-				key = {indexQ}> <p className=" m-1 p-3 mb-2 bg-primary text-white">{elem.question}</p>
+                <ol className={this.props.viewQuestions[indexQ] == true ? "d-block" : "d-none" }
+                key = {indexQ}> <p className=" m-1 p-3 mb-2 bg-primary text-white">{elem.question}</p>
                 <div>
                 {elem.answers.map((elem, indexA)=>{
                     return (
-                    <li className='list-group-item' key={indexA}> 
-					<p><input 
-					type = 'checkbox'  
-					className='mr-1' 
-					onChange={this.props.handleInp.bind (null, indexQ, indexA)} 
-					value={indexA}
-					selected={this.props.inputsVal[indexQ][indexA]== true}					
-					/>
+                    <li className='list-group-item' key={indexA}>
+                    <p><input
+                    type = 'checkbox'
+                    className='mr-1'
+                    onChange={this.props.handleInp.bind (null, indexQ, indexA)}
+                    value={indexA}
+                    selected={this.props.inputsVal[indexQ][indexA]== true}
+                    />
                     {elem}</p>
                     </li>
                     )
@@ -77,134 +77,172 @@ constructor() {
             {question: 'вопрос 4', answers: ['вариант 1','вариант 2','вариант 3','вариант 4'], rightAnswer : [1,3],
                 button: <p className='m-3'> Возможно несколько вариантов.</p>,
             next:<button onClick={this.next.bind(this, 3)} className = {'btn btn-outline-primary'}>Вперед</button>, back:<button className = {'btn btn-outline-primary'} onClick={this.back.bind(this, 3)}>Назад</button>}
-            ], inputsVal: [[],[],[],[]], viewQuestions:[true, false, false, false], check: false, 
-			rigthA:[[false,true,false,true],[false,true,false,true],[false,true,false,true],[false,true,false,true]]};
+            ], inputsVal: [[],[],[],[]], viewQuestions:[true, false, false, false], check: false,
+            rigthA:[[false,true,false,true],[false,true,false,true],[false,true,false,true],[false,true,false,true]]};
 
         }
 
         handleInp(indexQ, indexA){
-			
-			if (this.state.inputsVal[indexQ][indexA] == undefined) {
-				
-				this.state.inputsVal[indexQ][indexA] = true
-				this.setState ({inputsVal:this.state.inputsVal})
-			} else {
-				
-				this.state.inputsVal[indexQ][indexA] = !this.state.inputsVal[indexQ][indexA]
-				this.setState ({inputsVal:this.state.inputsVal})
-				
-				}
-			
-			var count = 0;
-					
-				for (var i = 0; i<this.state.inputsVal.length;i++) {
-					
-					if (this.state.inputsVal[i].length == 0) {
-							count++;
-						}
-				}	
-					   
-                 
-				if (count == 0) {
+
+            if (this.state.inputsVal[indexQ][indexA] == undefined) {
+
+                this.state.inputsVal[indexQ][indexA] = true
+                this.setState ({inputsVal:this.state.inputsVal})
+            } else {
+
+                this.state.inputsVal[indexQ][indexA] = !this.state.inputsVal[indexQ][indexA]
+                this.setState ({inputsVal:this.state.inputsVal})
+
+                }
+
+            var count = 0;
+
+                for (var i = 0; i<this.state.inputsVal.length;i++) {
+
+                    if (this.state.inputsVal[i].length == 0) {
+                            count++;
+                        }
+                }
+
+
+                if (count == 0) {
                     this.state.check=true
                     this.setState ({check:this.state.check})
                     }
-        		
-		   }
+
+           }
 
         doneTest () {
-					
-					if(Array.prototype.equals)
-							console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-						// attach the .equals method to Array's prototype to call it on any array
-						Array.prototype.equals = function (array) {
-							// if the other array is a falsy value, return
-							if (!array)
-								return false;
 
-							// compare lengths - can save a lot of time 
-							if (this.length != array.length)
-								return false;
+                    if(Array.prototype.equals)
+                            console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+                        // attach the .equals method to Array's prototype to call it on any array
+                        Array.prototype.equals = function (array) {
+                            // if the other array is a falsy value, return
+                            if (!array)
+                                return false;
 
-							for (var i = 0, l=this.length; i < l; i++) {
-								// Check if we have nested arrays
-								if (this[i] instanceof Array && array[i] instanceof Array) {
-									// recurse into the nested arrays
-									if (!this[i].equals(array[i]))
-										return false;       
-								}           
-								else if (this[i] != array[i]) { 
-									// Warning - two different object instances will never be equal: {x:20} != {x:20}
-									return false;   
-								}           
-							}       
-						return true;
-					}
-					
-					Object.defineProperty(Array.prototype, "equals", {enumerable: false});
-					
-					var arr = this.state.inputsVal
-					
-					
-					for (var i = 0; i<arr.length;i++) {
-						
-						
-						for (var j = 0; j<arr[i].length;j++) {
-							
-							if (arr[i][j] == undefined) {
-								
-								arr[i][j] = false;
-							}
-							
-						}
-					}
-					
-					for (var i = 0; i<arr.length;i++) {
-					
-						if (arr[i].length < 4) {
-						
-							var l = 4-arr[i].length
-					
-					for (var k = 0; k<l;k++) {
-							arr[i].push (false)
-							}
-							
-						}
-						
-					}
-					
-				
-					
-				// вывод правильных ответов на экран пользователю
-		
-				// массив ответов и массив ответов пользователя
+                            // compare lengths - can save a lot of time
+                            if (this.length != array.length)
+                                return false;
 
-				// свои ответы
-		
-		
+                            for (var i = 0, l=this.length; i < l; i++) {
+                                // Check if we have nested arrays
+                                if (this[i] instanceof Array && array[i] instanceof Array) {
+                                    // recurse into the nested arrays
+                                    if (!this[i].equals(array[i]))
+                                        return false;
+                                }
+                                else if (this[i] != array[i]) {
+                                    // Warning - two different object instances will never be equal: {x:20} != {x:20}
+                                    return false;
+                                }
+                            }
+                        return true;
+                    }
+
+                    Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+                    var arr = this.state.inputsVal
+
+
+                    for (var i = 0; i<arr.length;i++) {
+
+
+                        for (var j = 0; j<arr[i].length;j++) {
+
+                            if (arr[i][j] == undefined) {
+
+                                arr[i][j] = false;
+                            }
+
+                        }
+                    }
+
+                    for (var i = 0; i<arr.length;i++) {
+
+                        if (arr[i].length < 4) {
+
+                            var l = 4-arr[i].length
+
+                    for (var k = 0; k<l;k++) {
+                            arr[i].push (false)
+                            }
+
+                        }
+
+                    }
+
+
+
+                // вывод правильных ответов на экран пользователю
+
+                // массив ответов и массив ответов пользователя
+
+                // свои ответы
+
+
             for (let i =0 ; i<this.state.questions.length; i++) {
-						
-						
-					
-					alert (this.state.inputsVal[i].equals(this.state.rigthA[i]))
-					
+
+
                 if (this.state.inputsVal[i].equals(this.state.rigthA[i]) ) {
-						
-						const rightAnswerV = this.state.questions[i].rightAnswer.map ((elem,index)=>{
-								return (<p style={{color: 'green'}}>ваш ответ {this.state.questions[i].answers[elem]}  И это правильно</p>)
-								
-							})
-						
-                        this.state.questions[i].button = rightAnswerV
+
+                        const rightAnswerV = this.state.questions[i].rightAnswer.map ((elem,index)=>{
+                                return (<p style={{color: 'green'}}>правильный ответ {this.state.questions[i].answers[elem]}</p>)
+
+                            })
+
+                   const userAns = this.state.inputsVal[i]. map ((e,i)=>{
+                                    var arr = []
+                                    if (e == true) {
+                                        arr.push(i)
+                                        }
+
+                                        const ansVal = arr.map ((e,i)=>{
+                                            return (<p>{this.state.questions[i].answers[e]}</p>)
+
+                                            })
+
+                                        return ansVal
+                                    })
+
+                        const res = <div>  ПОЗДАРАВЛЯЕМ, ВЕРНО
+                                    <div> Ваши ответы {userAns}</div>
+                                    <div> {rightAnswerV}</div>
+                                    </div>
+
+                        this.state.questions[i].button = res
                         this.setState ({questions:this.state.questions})
 
 
                     } else {
-						
-						const wrongAnswerV = this.state.questions[i].rightAnswer.map ((elem,index)=>{
-								return (<p style={{color: 'red'}}>ваш ответ 1111111 а правильный {this.state.questions[i].answers[elem]}</p>)
-								
-							})
+
+                         const userAns = this.state.inputsVal[i]. map ((e,i)=>{
+                                    var arr = []
+                                    if (e == true) {
+                                        arr.push(i)
+                                        }
+
+                                        const ansVal = arr.map ((e,i)=>{
+                                            return (<p>{this.state.questions[i].answers[e]}</p>)
+
+                                            })
+
+                                        return ansVal
+                                    })
+
+                        const rightAnswerV1 = this.state.questions[i].rightAnswer.map ((elem,index)=>{
+                                return (<p style={{color: 'green'}}>правильный ответ {this.state.questions[i].answers[elem]}</p>)
+
+                            })
+
+
+                        const wrongAnswerV = <div>
+                                                НЕ ВЕРНО
+                                            <div>Ваши ответы{userAns}</div>
+                                            <div> {rightAnswerV1} </div>
+
+                                            </div>
 
                         this.state.questions[i].button = wrongAnswerV;
                         this.setState ({questions:this.state.questions})
@@ -226,7 +264,7 @@ constructor() {
                 this.setState ({questions:this.state.questions})
                 this.setState ({viewQuestions:this.state.viewQuestions})
 
-		}
+        }
 
             back (num){
 
@@ -240,7 +278,7 @@ constructor() {
                 this.state.viewQuestions[back] = !this.state.viewQuestions[back]
                 this.setState ({viewQuestions:this.state.viewQuestions})
 
-              
+
             }
             next (num){
 
@@ -255,10 +293,10 @@ constructor() {
                 this.state.viewQuestions[next] = !this.state.viewQuestions[next]
                 this.setState ({viewQuestions:this.state.viewQuestions})
 
-                
-				}
-				
-               
+
+                }
+
+
 
     render () {
 
@@ -274,7 +312,7 @@ constructor() {
             question={this.state.questions}
             handleInp={this.handleInp.bind(this)}
             viewQuestions={this.state.viewQuestions}
-			inputsVal={this.state.inputsVal}
+            inputsVal={this.state.inputsVal}
 
             />
             <div>{this.state.check == true? questionsDone:""}</div>
